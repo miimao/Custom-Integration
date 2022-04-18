79f1b262dd52243420f16c9810ac260502527094
Joseph Gaumond <joseph@ctcatlanta.net>
2022-04-18 03:03:08 -0400

    shortend cwmanage_api_functions (add_configuration_to_ticket)

--- cwmanage_api_functions.py
+++ cwmanage_api_functions.py
@@ -7,6 +7,7 @@ import re
 regex_ip = re.compile('''((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)''')
 regex_mac = re.compile(r'(?:[0-9a-fA-F]:?){12}')
 regex_device_name = re.compile(r'(?<=Device Name: ).*')
+regex_domotz_device_id = re.compile(r'/devices/(.*?)\?notify=ticketing')
 
 #get a dictionary with cw manage bopard names and there id's
 def get_all_cw_manage_board_id():
@@ -45,66 +46,49 @@ def get_testing_ticket():
 #add a configuration to a ticket/s if it does not already have one.
 def add_configuration_to_ticket(ticket):
     for ticket in ticket:
+        checked_tickets = []
         ticket_id = ticket["id"]
         #Sometimes a company name will have a " ' " in its name, we want to get rid of these or they cause errors
         ticket_company = ticket['company']['name'].replace("'","\\'")
         #Checking to see if we already have a configuration assigned to the ticket.
         ticket_configurations = requests.get(url=ticket['_info']['configurations_href'],headers=constants.headers_cw).json()
         if ticket_configurations == []:
-            config_set = False
-            print(f"\nTicket:{ticket['id']} - No configuration assaigned! Attempting to find one.")
+            print(f"\nTicket:{ticket['id']} ({ticket_company}) - No configuration assaigned! Attempting to find one.")
             #Get the initial note so we can parse it for information we can use to find the configuration.
-            ticket_note = requests.get(headers=constants.headers_cw, url=f"https://api-na.myconnectwise.net/v2021_3/apis/3.0/service/tickets/{ticket_id}/allNotes", params={"conditions":"noteType='TicketNote'","orderBy":"id asc","pageSize":"1","fields":"text"}).json()[0]["text"]
+            ticket_note = requests.get(headers=constants.headers_cw, url=f"{constants.cw_manage_url}/service/tickets/{ticket_id}/allNotes", params={"conditions":"noteType='TicketNote'","orderBy":"id asc","pageSize":"1","fields":"text"}).json()[0]["text"]
             # print(ticket_note)
+            regex_parse = {}
             try:
-                mac_address = re.search(regex_mac,ticket_note).group()
+                regex_parse['domotz_id'] = re.search(regex_domotz_device_id,ticket_note).group(1)
             except:
-                mac_address = None
+                regex_parse['domotz_id']  = None
             try:
-                ip_address = re.search(regex_ip,ticket_note).group()
+                regex_parse['macAddress'] = re.search(regex_mac,ticket_note).group()
             except:
-                ip_address = None
+                regex_parse['macAddress'] = None
             try:
-                device_name = re.search(regex_device_name,ticket_note).group()
+                 regex_parse['ipAddress'] = re.search(regex_ip,ticket_note).group()
             except:
-                device_name = None
-            print(f"Ticket:{ticket_id} - Found (NAME: {device_name} - MAC: {mac_address} - IP: {ip_address})")
-            #using the information we can search for a configuration that matches any of the data.
-            if mac_address != None:
-                try:
-                    configuration_id = requests.get(headers=constants.headers_cw, url=f"https://api-na.myconnectwise.net/v2021_3/apis/3.0/company/configurations/", params={"conditions":f"macAddress='{mac_address}' AND company/name='{ticket_company}'","fields":"id"}).json()
-                    if configuration_id != []:
-                        configuration_id = configuration_id[0]
-                        config_post_response = requests.post(url=f"https://api-na.myconnectwise.net/v2021_3/apis/3.0/service/tickets/{ticket_id}/configurations",headers=constants.headers_cw,data=f"{configuration_id}")
-                        print(f"Set Configuration - ID:{configuration_id['id']} ({config_post_response.json()['_info']['name']}) for Ticket: {ticket_id},")
-                        config_set = True
-                    else:
-                        print(f"Ticket:{ticket_id} - Unable to find a configuration Based on the Mac Address: {mac_address}")
-                except:
-                    print(f"An exception occurred. (MAC CHECK) Ticket ID {ticket_id}")
-            if ip_address != None and config_set != True:
-                try:
-                    configuration_id = requests.get(headers=constants.headers_cw, url=f"https://api-na.myconnectwise.net/v2021_3/apis/3.0/company/configurations/", params={"conditions":f"ipAddress='{ip_address}' AND company/name='{ticket_company}'","fields":"id"}).json()
-                    if configuration_id != []:
-                        configuration_id = configuration_id[0]
-                        config_post_response = requests.post(url=f"https://api-na.myconnectwise.net/v2021_3/apis/3.0/service/tickets/{ticket_id}/configurations",headers=constants.headers_cw,data=f"{configuration_id}")
-                        print(f"Set Configuration - ID:{configuration_id['id']} ({config_post_response.json()['_info']['name']}) for Ticket: {ticket_id},")
-                        config_set = True
-                    else:
-                        print(f"Ticket:{ticket_id} - Unable to find a configuration Based on the IP Address: {ip_address}")
-                except:
-                    print(f"An exception occurred. (IP CHECK) Ticket ID {ticket_id}")
-            if device_name != None and config_set != True:
-                try:
-                    configuration_id = requests.get(headers=constants.headers_cw, url=f"https://api-na.myconnectwise.net/v2021_3/apis/3.0/company/configurations/", params={"conditions":f"name='{device_name}' AND company/name='{ticket_company}'","fields":"id"}).json()
-                    if configuration_id != []:
-                        configuration_id = configuration_id[0]
-                        config_post_response = requests.post(url=f"https://api-na.myconnectwise.net/v2021_3/apis/3.0/service/tickets/{ticket_id}/configurations",headers=constants.headers_cw,data=f"{configuration_id}")
-                        print(f"Set Configuration - ID:{configuration_id['id']} ({config_post_response.json()['_info']['name']}) for Ticket: {ticket_id},")
-                        config_set = True
-                    else:
-                        print(f"Ticket:{ticket_id} - Unable to find a configuration Based on the Device Name: {device_name}")
-                except:
-                    print(f"An exception occurred. (NAME CHECK) Ticket ID {ticket_id}")
+                regex_parse['ipAddress'] = None
+            try:
+                regex_parse['name'] = re.search(regex_device_name,ticket_note).group()
+            except:
+                regex_parse['name'] = None
+            for i in regex_parse:
+                if regex_parse[i] != None:
+                    try:
+                        if i == "domotz_id":
+                            configuration_id = requests.get(headers=constants.headers_cw, url=f"{constants.cw_manage_url}/company/configurations/", params={"customFieldConditions":f"caption='Domotz ID' AND value={regex_parse[i]}","fields":"id"}).json()
+                        else:
+                            configuration_id = requests.get(headers=constants.headers_cw, url=f"{constants.cw_manage_url}/company/configurations/", params={"conditions":f"{i}='{regex_parse[i]}' AND company/name='{ticket_company}'","fields":"id"}).json()
+                        if configuration_id != []:
+                            configuration_id = configuration_id[0]
+                            config_post_response = requests.post(url=f"{constants.cw_manage_url}/service/tickets/{ticket_id}/configurations",headers=constants.headers_cw,data=f"{configuration_id}")
+                            print(f"Set Configuration - ID:{configuration_id['id']} ({config_post_response.json()['_info']['name']}) for Ticket: {ticket_id}, Based on: {i} ({regex_parse[i]})")
+                            break
+                        else:
+                            print(f"Ticket:{ticket_id} - Unable to find a configuration Based on the {i}: {regex_parse[i]}")
+                    except:
+                        print(f"An exception occurred. ({i}) Ticket ID {ticket_id}")
         else:
             print(f"\nTicket:{ticket_id} for ({ticket_company}) - is already assigned a configuration. ({ticket_configurations[0]['_info']['name']})")
--- main.py
+++ main.py
@@ -7,14 +7,19 @@ import constants
 import cwmanage_api_functions
 import domotz_api_functions
 
-
-
+#---Get a list of open tickets created by the "DomtozAPI"---
 test_ticket = cwmanage_api_functions.get_testing_ticket()
 open_tickets = cwmanage_api_functions.get_open_domotz_tickets()
 domotz_agents = domotz_api_functions.get_all_domotz_agents_id()
+checked_tickets = []
+print(open_tickets)
 
 
-cwmanage_api_functions.add_configuration_to_ticket(test_ticket)
+# print(domotz_agents)
+while True:
+    open_tickets = cwmanage_api_functions.get_open_domotz_tickets()     #Get a list of open tickets made by "DomotzAPI"
+    cwmanage_api_functions.add_configuration_to_ticket(open_tickets)    #Attempt to add a configuration to these tickets
+    #
 
 
 
