cfbb26d09b26949f068308ea0d23cc1c2a925cea
Joseph Gaumond <joseph@ctcatlanta.net>
2022-04-25 17:34:48 -0400

    Added function to return a list of domotz devices that have no cw manage configuration

--- domotz_api_functions.py
+++ domotz_api_functions.py
@@ -14,6 +14,44 @@ def get_all_domotz_agents_id():
     return agents_ids
 
 
-# Check for out of date information in a cw_manage configuration
-def check_cwconfig_against_domotz(configuration):
-    pass
+# Get all Devices listed in a given Domotz Agent
+def get_all_devices_from_domotz_agent(agent_id):
+    url = constants.domotz_url + f"/agent/{agent_id}/device"
+    devices_list = requests.get(url=url, headers=constants.headers_domotz).json()
+    for device in devices_list:
+        device["agent_id"] = agent_id
+    return devices_list
+
+
+# Make a list of all domotz devices that do not currently have a cwmanage configuration
+def get_noconfig_domotz_devices(list_of_cw_configurations):
+    domotz_agents = get_all_domotz_agents_id()
+    domotz_devices = []
+    domotz_devices_no_config = []
+    # Get a list of all Domotz Devices
+    for agent in domotz_agents:
+        domotz_devices.extend(get_all_devices_from_domotz_agent(domotz_agents[agent]))
+    # Strip Everything but the cw config id, domotz id, and the property name out of the configuration info
+    list_filtered_configs = []
+    for config in list_of_cw_configurations:
+        filtered_config = {}
+        filtered_config["cw_config_id"] = config["id"]
+        # we probbaly dont need the domotz agent id but im leaving it here incase I find out the device id is not unique
+        filtered_config["domotz_agent_id"] = domotz_agents[config["company"]["name"]]
+        filtered_config["domotz_id"] = int(
+            list(
+                filter(
+                    lambda domotz_info: domotz_info["id"] == 12, config["customFields"]
+                )
+            )[0]["value"]
+        )
+        list_filtered_configs.append(filtered_config)
+    # get a list of only devices that we cannot find a matching id for in and remove any device in this list that is not marked as "importance": "VITAL"
+    domotz_devices_no_config = list(
+        filter(
+            lambda device: device["id"] != filtered_config["domotz_id"]
+            and device["importance"] == "VITAL",
+            domotz_devices,
+        )
+    )
+    return domotz_devices_no_config
--- main.py
+++ main.py
@@ -22,23 +22,25 @@ domotz_agents = domotz_api_functions.get_all_domotz_agents_id()
 checked_tickets = []
 # print(open_tickets)
 
-
-# print(domotz_agents)
-while True:
-    time.sleep(5)
-    # Get a list of open tickets made by "DomotzAPI" and then remove the tickets that have already been checked
-    open_tickets = [
-        ticket
-        for ticket in cwmanage_api_functions.get_open_domotz_tickets()
-        if ticket not in checked_tickets
-    ]
-
-    # Attempt to add a configuration to these tickets
-    cwmanage_api_functions.add_configuration_to_ticket(open_tickets)
-    # Add these tickets to the list of already proccessed tickets
-    checked_tickets.extend(open_tickets)
+test_configs = cwmanage_api_functions.get_all_configs_with_domotz_id()
+domotz_api_functions.find_missing_domotz_devices(test_configs)
 
 
-# for ticket in test_tickets:
-#     print(ticket["id"])
-#     print(domotz_agents[ticket["company"]["name"]])
+# # print(domotz_agents)
+# while True:
+#     time.sleep(5)
+#     # Get a list of open tickets made by "DomotzAPI" and then remove the tickets that have already been checked
+#     open_tickets = [
+#         ticket
+#         for ticket in cwmanage_api_functions.get_open_domotz_tickets()
+#         if ticket not in checked_tickets
+#     ]
+
+#     # Attempt to add a configuration to these tickets
+#     cwmanage_api_functions.add_configuration_to_ticket(open_tickets)
+#     # Add these tickets to the list of already proccessed tickets
+#     checked_tickets.extend(open_tickets)
+
+# Look for missing domotz devices in all the configurations for a property
+# for property in domotz_agents:
+#     pass
